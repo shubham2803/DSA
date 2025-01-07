ce55b1aa1959986fec3fcb3a7dd60ef922453523
shubham <shubhamkumar421@gmail.com>
2025-01-07 21:40:41 +0530

    Leetcode #135. Candy (Peak and Valley)

--- leetcode150/135_candy.py
+++ leetcode150/135_candy.py
@@ -4,20 +4,25 @@ from typing import List
 
 
 def candy(ratings: List[int]) -> int:
-    i = 0
-    j = 1
-    out_list = [1] * len(ratings)
-    while j < len(ratings):
-        if ratings[j] > ratings[i]:
-            out_list[j] = max(out_list[i], out_list[i] + 1)
-        i += 1
-        j += 1
-    i = len(ratings) - 1
-    j = i - 1
+    n = len(ratings)
+    i = 1
+    out = n
+    peak = 0
+    valley = 0
+    while i < n:
+        if ratings[i] == ratings[i - 1]:
+            i += 1
 
-    while i > 0:
-        if ratings[j] > ratings[i]:
-            out_list[j] = max(out_list[j], out_list[i] + 1)
-        i -= 1
-        j -= 1
-    return sum(out_list)
+        peak = 0
+        while i < n and ratings[i] > ratings[i - 1]:
+            peak += 1
+            out += peak
+            i += 1
+        valley = 0
+        while i < n and ratings[i] < ratings[i - 1]:
+            valley += 1
+            out += valley
+            i += 1
+
+        out -= min(peak, valley)
+    return out
