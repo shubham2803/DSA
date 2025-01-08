ea20599f9492499134020d7a2da348031ece4fb2
shubham <shubhamkumar421@gmail.com>
2025-01-08 21:04:22 +0530

    Leetcode #3042. Count Prefix and Suffix Pairs I

--- leetcode/3042_count_prefix_and_suffix_pairs_I.py
+++ leetcode/3042_count_prefix_and_suffix_pairs_I.py
@@ -1,20 +1,12 @@
 # https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/description/?envType=daily-question&envId=2025-01-08
+from typing import List
+
 
 def countPrefixSuffixPairs(self, words: List[str]) -> int:
     def isPrefixAndSuffix(str1, str2):
-        concat = str1 + ':' + str2
-        print(concat)
-        n = len(concat)
-        j = 0
-        lps = [0] * n
-        for i in range(1, n):
-            while j > 0 and concat[i] != concat[j]:
-                j = lps[j - 1]
-            if concat[i] == concat[j]:
-                j += 1
-            lps[i] = j
-        m = len(str1)
-        return (lps[-1] == m) and (lps[m: (2 * m) + 1][-1] == m)
+        if str2.startswith(str1) and str2.endswith(str1):
+            return True
+        return False
 
     w = len(words)
     out = 0
