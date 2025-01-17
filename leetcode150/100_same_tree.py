561652477d713d921c33ff84d02034ef4590983e
Shubham Kumar Singh <shubhamkumar421@gmail.com>
2025-01-17 20:12:22 +0530

    Leetcode #100. Same Tree

--- leetcode150/100_same_tree.py
+++ leetcode150/100_same_tree.py
@@ -15,4 +15,4 @@ def isSameTree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
     if not p or not q or p.val != q.val:
         return False
 
-    return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+    return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
