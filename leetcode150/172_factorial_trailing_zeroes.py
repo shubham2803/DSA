af61228ec1885d0357c713a9530f39fd5f9f2cee
shubham <shubhamkumar421@gmail.com>
2025-01-22 21:15:28 +0530

    Leetcode #172. Factorial Trailing Zeroes

---
+++ leetcode150/172_factorial_trailing_zeroes.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/factorial-trailing-zeroes/?envType=study-plan-v2&envId=top-interview-150
+
+def trailingZeroes(n: int) -> int:
+    count = 0
+    while n >= 5:
+        n //= 5
+        count += n
+    return count
