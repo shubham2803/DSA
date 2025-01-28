883f588937736fa050d3691f58992e31112e62ea
shubham <shubhamkumar421@gmail.com>
2025-01-28 21:12:00 +0530

    Leetcode #42. Trapping Rain Water

---
+++ leetcode150/42_trapping_rain_water.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/trapping-rain-water/?envType=study-plan-v2&envId=top-interview-150
+from typing import List
+
+
+def trap(height: List[int]) -> int:
+    if not height:
+        return 0
+
+    left, right = 0, len(height) - 1
+    left_max, right_max = 0, 0
+    water_trapped = 0
+
+    while left < right:
+        if height[left] < height[right]:
+            if height[left] >= left_max:
+                left_max = height[left]
+            else:
+                water_trapped += left_max - height[left]
+            left += 1
+        else:
+            if height[right] >= right_max:
+                right_max = height[right]
+            else:
+                water_trapped += right_max - height[right]
+            right -= 1
+
+    return water_trapped
