a4d2dcecbd42b8d46fcb40966b8accc37ab62aa9
Shubham Kumar Singh <shubhamkumar421@gmail.com>
2025-02-12 22:27:10 +0530

    Leetcode #2342. Max Sum of a Pair With Equal Sum of Digits (Optimised)

--- leetcode/2342_max_sum_of_a_pair_with_equal_sum_of_digits.py
+++ leetcode/2342_max_sum_of_a_pair_with_equal_sum_of_digits.py
@@ -12,14 +12,19 @@ def maximumSum(self, nums: List[int]) -> int:
             digit_sum += remain
             x = x // 10
         if digit_sum not in data_dict:
-            data_dict[digit_sum] = [val]
+            data_dict[digit_sum] = [val, -1]
         else:
-            data_dict[digit_sum].append(val)
+            if val > data_dict[digit_sum][0]:
+                data_dict[digit_sum][1] = data_dict[digit_sum][0]
+                data_dict[digit_sum][0] = val
+            elif val > data_dict[digit_sum][1]:
+                data_dict[digit_sum][1] = val
 
     max_sum = -1
     for k, v in data_dict.items():
-        v = sorted(v)[::-1]
-        if len(v) >= 2:
-            max_sum = max(sum(v[:2]), max_sum)
+        if v[-1] != -1:
+            max_sum = max(sum(v), max_sum)
     return max_sum
+
+
 
